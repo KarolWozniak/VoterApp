252e1e69176ea68aea68b3dcbb5c248d61b67a98
KarolWozniak <s165308@student.pg.gda.pl>
2018-09-27 18:24:07 +0200

    Adding generic views

--- voter/urls.py
+++ voter/urls.py
@@ -1,11 +1,10 @@
+
 from django.urls import path
 
 from voter import views
 
 urlpatterns = [
-    path('<int:voting_id>/', views.get_voting, name='detail'),
-    path('edit/', views.post_voting, name='edit'),
-    path('edit/add', views.add_voting, name='add'),
-    path('<int:voting_id>/vote/', views.vote, name='vote'),
-    path('<int:voting_id>/monitor/', views.monitor, name='monitor')
+    path('edit/', views.EditVotingView.as_view(), name='edit'),
+    path('<pk>/', views.VotingDetailView.as_view(), name='detail'),
+    path('<pk>/monitor/', views.MonitorVotingDetailView.as_view(), name='monitor'),
 ]
--- voter/views.py
+++ voter/views.py
@@ -1,42 +1,38 @@
-from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic import DetailView
+from django.views import View
 
 from voter.models import Voting, Vote, Option
 
 
-def get_voting(req, voting_id):
-    voting = get_object_or_404(Voting.objects, pk=voting_id)
-    context = {
-        'voting': voting,
-        'user': 'temp_user'
-    }
-    return render(req, 'voter/index.html', context)
+class VotingDetailView(DetailView):
+    template_name = 'voter/index.html'
+    model = Voting
 
-
-def post_voting(req):
-    return render(req, 'voter/editor.html')
-
-
-def vote(req, voting_id):
-    if req.method == "POST":
+    def post(self, req, pk):
         option = req.POST.get('option')
-    voting = get_object_or_404(Voting.objects, pk=voting_id)
-    vote_obj = Vote(user_name='user', option_num=option, voting_id=voting)
-    vote_obj.save()
-    return JsonResponse({'response': 'OK'})
+        voting = get_object_or_404(Voting.objects, pk=pk)
+        vote_obj = Vote(user_name='user', option_num=option, voting_id=voting)
+        vote_obj.save()
+        return redirect('monitor', pk=pk)
 
 
-def monitor(req, voting_id):
-    voting = get_object_or_404(Voting.objects, pk=voting_id)
-    return render(req, 'voter/monitor.html', {'voting': voting})
+class MonitorVotingDetailView(DetailView):
+    template_name = 'voter/monitor.html'
+    model = Voting
 
 
-def add_voting(req):
-    voting = Voting.objects.create(author='temp_user')
-    voting.save()
-    if req.method == "POST":
+class EditVotingView(View):
+
+    def get(self, req):
+        return render(req, 'voter/editor.html')
+
+    def post(self, req):
+        voting = Voting.objects.create(author='temp_user')
+        voting.save()
         option_list = req.POST.getlist('option')
         for index, value in enumerate(option_list):
             temp = Option.objects.create(voting_id=voting, content=value, number=index)
             temp.save()
-    return redirect('monitor', voting_id=voting.id)
+        return redirect('monitor', pk=voting.id)
+
