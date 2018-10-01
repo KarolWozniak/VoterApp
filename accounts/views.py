fa813b0cfc8ebc22e047de46fb51f92169c88b23
KarolWozniak <s165308@student.pg.gda.pl>
2018-10-01 16:26:36 +0200

    Registration and logging

---
+++ accounts/views.py
@@ -0,0 +1,25 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.views import View
+
+from accounts import forms
+
+
+class RegisterView(View):
+
+    def get(self, req):
+        form = forms.UserForm()
+        return render(req, 'accounts/register.html', {'form': form})
+
+    def post(self, req):
+        form = forms.UserForm(req.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+            user = authenticate(username=username, password=password)
+            login(req, user)
+            return redirect('/voter/edit')
+        return render(req, 'accounts/register.html', {'form': form})
--- website/urls.py
+++ website/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('voter/', include('voter.urls'))
+    path('voter/', include('voter.urls')),
+    path('accounts/', include('accounts.urls'))
 ]
