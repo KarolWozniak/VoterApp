d2a28aa209cd403fbb379bba1fbd54d8afe8420c
Karol Wozniak <karol.wozniak@ihsmarkit.com>
2020-06-22 21:32:11 +0200

    Changing urls

--- voter/urls.py
+++ voter/urls.py
@@ -5,7 +5,7 @@ from voter import views
 
 urlpatterns = [
     path('edit/', views.EditVotingView.as_view(), name='edit'),
-    path('homepage/', views.HomepageView.as_view(), name='homepage'),
+    path('', views.HomepageView.as_view(), name='homepage'),
     path('info/', views.InfoListView.as_view(), name='info'),
     path('<pk>/', views.VotingDetailView.as_view(), name='detail'),
     path('<pk>/monitor/', views.MonitorVotingDetailView.as_view(), name='monitor'),
